f15fa452732695d13259ae0bfa66910e654f5642
harolte <harolte@hotmail.com>
2024-08-13 17:09:18 -0500

    actualizando microapp

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Header from "./Header";
 const App = () => (
   <div className="text-3xl mx-auto max-w-6xl">
     <div className="text-center">
+    <Header />
       <img
         src="https://i.ibb.co/CVYFJBM/avatar.webp"
         className="rounded-full w-32 mb-4 mx-auto"
@@ -14,8 +15,12 @@ const App = () => (
       />
       <h5 className="text-xl font-medium leading-tight mb-2">Hola Mundo</h5>
       <p className="text-gray-500">Harolt kruchinsky</p>
+      <Footer />
     </div>
   </div>
+
+
+
 );
 
 const root = ReactDOM.createRoot(document.getElementById("app"));
